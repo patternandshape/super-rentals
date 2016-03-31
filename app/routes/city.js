64fbf44a51cde24649ee0a34d40f2f60eaaa8708
Molly Curtin and Paul Hess <pauljacksonhess@gmail.com>
2016-03-31 09:57:35 -0700

    add working delete city function yes

--- app/routes/city.js
+++ app/routes/city.js
@@ -5,14 +5,25 @@ export default Ember.Route.extend({
     return this.store.findRecord('city', params.city_id);
   },
   actions: {
-  save3(params) {
-  var newRental = this.store.createRecord('rental', params);
-  var city = params.city;
-  city.get('rentals').addObject(newRental);
-  newRental.save().then(function() {
-    return city.save();
-  });
-  this.transitionTo('city', params.city);
-}
-}
+    save3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      var city = params.city;
+      city.get('rentals').addObject(newRental);
+      newRental.save().then(function() {
+        return city.save();
+      });
+      this.transitionTo('city', params.city);
+    },
+    delete(city) {
+      if (confirm('Are you sure you want to delete this city?')){
+        var rental_deletions = city.get('rentals').map(function(rental) {
+        return rental.destroyRecord();
+        });
+        Ember.RSVP.all(rental_deletions).then(function(){
+          return city.destroyRecord();
+        });
+        this.transitionTo('index');
+      }
+    }
+  }
 });
